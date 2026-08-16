86cc17615905d4a359985f33cbda0366e6d56b19
CODE-NUBAID <nubaid.uddin2007@gmail.com>
2026-08-17 00:13:29 +0530

    Update comment for unique names query
    Clarified comment about the query for unique medicine names.

--- backend/database.py
+++ backend/database.py
@@ -100,7 +100,7 @@ def get_all_medicine_names():
         conn = get_connection()
         cursor = conn.cursor()
 
-        # Taking out unique names
+        # Taking out unique names with the help of the below query
         query = "SELECT DISTINCT medicine_name FROM medicines"
         cursor.execute(query)
         
